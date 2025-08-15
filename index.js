64f982913a0380f5f778c3d13a0704e433ba647e
Vedant1607 <sinhavedant0705@gmail.com>
2025-08-15 19:10:22 +0530

    add admin route

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { userRouter } from './routes/user.js';
 import { courseRouter } from './routes/course.js';
-
+import { adminRouter } from "./routes/admin.js";
 
 const app = express();
 
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
+app.use("/admin", adminRouter);
 
 app.listen(3000, () => {
   console.log("App is running");
---
+++ routes/admin.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.post('/signup', (req, res) => {
+    res.send({
+        message:"signup endpoint",
+    })
+});
+
+adminRouter.post('/signin', (req, res) => {
+    res.send({
+        message:"signin endpoint",
+    })
+});
+
+adminRouter.post("/course", (req, res) => {
+    res.json({
+        message:"course endpoint"
+    })
+});
+
+adminRouter.put('/course', (req, res) => {
+    res.json({
+        message:"course endpoint"
+    })
+});
+
+adminRouter.get('course/bulk', (req, res) => {
+    res.json({
+        message:"course bulk endpoint"
+    })
+});
